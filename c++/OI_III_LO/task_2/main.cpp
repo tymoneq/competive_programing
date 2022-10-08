b3a7feaa2b440395f8e438e8f174da657246338a
tymoneq <tymontumialis@gmail.com>
2022-10-08 11:58:04 +0200

    szachy

--- c++/OI_III_LO/task_2/main.cpp
+++ c++/OI_III_LO/task_2/main.cpp
@@ -40,44 +40,56 @@ int main()
         has_value.push_back(make_pair(jj, i));
     }
 
-    // for (int i = 0; i < m; i++)
-    // {
-    //     int max_col = 0;
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         if (max_col < V[j][i])
-    //         {
-    //             max_col = V[j][i];
-    //         }
-    //     }
-    //     max_cols.push_back(max_col);
-    // }
-    sort(has_value.begin(), has_value.end());
-    for (int i = 1; i < has_value.size(); i++)
+    for (int i = 0; i < m; i++)
     {
-        if (has_value[i].first == has_value[i - 1].first)
+        int max_col = 0;
+        for (int j = 0; j < n; j++)
         {
-            int a = has_value[i].second;
-            int b = has_value[i - 1].second;
-            if (max_rows[a] > max_rows[b])
+            if (max_col < V[j][i])
             {
-                max_rows[b] = 0;
+                max_col = V[j][i];
             }
-            else if (max_rows[a] < max_rows[b])
-            {
-                max_rows[a] = 0;
-            }
-            else if (max_rows[a] == max_rows[b])
-                max_rows[a] = 0;
         }
+        max_cols.push_back(max_col);
     }
+    // sort(has_value.begin(), has_value.end());
+    //  for (int i = 1; i < has_value.size(); i++)
+    //  {
+    //      if (has_value[i].first == has_value[i - 1].first)
+    //      {
+    //          int a = has_value[i].second;
+    //          int b = has_value[i - 1].second;
+    //          if (max_rows[a] > max_rows[b])
+    //          {
+    //              max_rows[b] = 0;
+    //          }
+    //          else if (max_rows[a] < max_rows[b])
+    //          {
+    //              max_rows[a] = 0;
+    //          }
+    //          else if (max_rows[a] == max_rows[b])
+    //              max_rows[a] = 0;
+    //      }
+    //  }
     int max_rowww = 0;
     int max_rowww_2 = 0;
     make_heap(max_rows.begin(), max_rows.end());
     max_rowww = max_rows.front();
     pop_heap(max_rows.begin(), max_rows.end());
     max_rowww_2 = max_rows.front();
-    cout << max_rowww + max_rowww_2 << "\n";
 
+    int max_col = 0;
+    int max_col_2 = 0;
+    make_heap(max_cols.begin(), max_cols.end());
+    max_col = max_cols.front();
+    pop_heap(max_cols.begin(), max_cols.end());
+    max_col_2 = max_cols.front();
+
+    if (max_col + max_col_2 > max_rowww + max_rowww_2)
+    {
+        cout << max_rowww + max_rowww_2 << "\n";
+    }
+    else if (max_col + max_col_2 <= max_rowww + max_rowww_2)
+        cout << max_col + max_col_2 << "\n";
     return 0;
 }
