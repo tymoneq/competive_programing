8d0223597aae3e389fd474f2d91fa060966fad65
tymoneq <tymontumialis@gmail.com>
2022-11-08 14:39:34 +0100

    08.11.2022

---
+++ c++/jd/08.11.2022/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "troj.h"
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ c++/jd/08.11.2022/troj.h
@@ -0,0 +1,8 @@
+class Point
+{
+public:
+    Point();
+    ~Point();
+
+private:
+};
