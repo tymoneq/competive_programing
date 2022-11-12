d0fa85698ee0252ba9df4462085878a57142fccb
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-12 17:30:02 +0100

    dynamic

--- c++/OI_III_LO/05.11.2022/kwat/main.cpp
+++ c++/OI_III_LO/05.11.2022/kwat/main.cpp
@@ -1,72 +1,32 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-inline bool sort_2(vector<int> &a, vector<int> &b)
+struct flowers
 {
-    return a[1] < b[1];
-}
-inline bool sort_3(vector<int> &a, vector<int> &b)
-{
-    return a[2] < b[2];
-}
+    int red;
+    int green;
+    int blue;
+};
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int C, Z, N, T, wynik_1 = 0, wynik_2 = 0, wynik_3 = 0;
+    int C, Z, N, T;
     cin >> C >> Z >> N >> T;
-    const int c = C, z = Z, n = N;
-    vector<vector<int>> V(T);
+    vector<flowers> kwat(T);
+    vector<vector<vector<int>>> Res(T+1);
     for (int i = 0; i < T; i++)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        V[i].push_back(a);
-        V[i].push_back(b);
-        V[i].push_back(c);
+        int c, z, n;
+        cin >> c >> z >> n;
+        flowers fl;
+        fl.red = c;
+        fl.green = z;
+        fl.blue = n;
+        kwat[i] = fl;
     }
-    make_heap(V.begin(), V.end());
-    for (int i = T - 1; i >= 0; i--)
-    {
-        if (V[i][0] <= C && V[i][1] <= Z && V[i][2] <= N)
-        {
-            wynik_1 += 1;
-            C -= V[i][0];
-            Z -= V[i][1];
-            N -= V[i][2];
-        }
-    }
-    sort_heap(V.begin(), V.end());
-    make_heap(V.begin(), V.end(), sort_2);
-    C = c;
-    Z = z;
-    N = n;
-    for (int i = T - 1; i >= 0; i--)
-    {
-        if (V[i][0] <= C && V[i][1] <= Z && V[i][2] <= N)
-        {
-            wynik_2 += 1;
-            C -= V[i][0];
-            Z -= V[i][1];
-            N -= V[i][2];
-        }
-    }
-    sort_heap(V.begin(), V.end());
-    make_heap(V.begin(), V.end(), sort_3);
-    C = c;
-    Z = z;
-    N = n;
-    for (int i = T - 1; i >= 0; i--)
-    {
-        if (V[i][0] <= C && V[i][1] <= Z && V[i][2] <= N)
-        {
-            wynik_3 += 1;
-            C -= V[i][0];
-            Z -= V[i][1];
-            N -= V[i][2];
-        }
-    }
-    cout << max(wynik_1, max(wynik_2, wynik_3));
+   
+    
     return 0;
 }
---
+++ c++/dynamic_programing/intermediate/done/knapsack_problem.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline int max(int a, int b) { return (a > b) ? a : b; }
+int knapStack(int W, vector<pair<int, int>> &obj, int n)
+{
+    vector<vector<int>> K(n + 1, vector<int>(W + 1));
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w=0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (obj[i - 1].second <= w)
+                K[i][w] = max(obj[i - 1].first + K[i - 1][w - obj[i - 1].second], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
+
+int main()
+{
+    int N, W;
+    cin >> N >> W;
+    vector<pair<int, int>> obj(N); // first val second weights
+    for (int i = 0; i < N; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        obj[i] = make_pair(a, b);
+    }
+    auto val = knapStack(W, obj, N);
+    cout << val;
+    return 0;
+}
