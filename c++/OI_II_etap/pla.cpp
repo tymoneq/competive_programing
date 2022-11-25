1b7631955380861ff26da6b4f21c1bb5767a1c40
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-25 18:44:22 +0100

    pla popraw czas

--- c++/OI_II_etap/pla.cpp
+++ c++/OI_II_etap/pla.cpp
@@ -56,13 +56,13 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, X, z, a, N, prev = 0, current = 0;
+    int n, X, z, a, N = 0, prev = 0, current = 0;
     cin >> n >> X >> z;
     //  wczytanie danych
     distances_between ds_b;
-    vector<pair<ull, int>> res; // first licznik second mianownik
     distances_between ds_left;
     distances_between ds_right;
+    pair<ull, int> res; // first licznik second mianownik
     ull nww_b_l, nww_b_r, l_m, b_m_1, r_m, b_m_2;
     priority_queue<distances_between, vector<distances_between>, myComp<distances_between>> pq;
     for (int i = 0; i < n; i++)
@@ -93,60 +93,57 @@ int main()
     for (int i = 0; i < z; i++)
     {
         cin >> a;
-        if (a <= res.size())
-            cout << res[a - 1].first << "/" << res[a - 1].second << "\n";
-        else
+        for (int j = 0; j < a - N; j++)
         {
-            N = res.size();
-            for (int j = 0; j < a - N; j++)
+            ds_b = pq.top();
+            if (ds_b.right_licznik == 0)
             {
-                ds_b = pq.top();
-                if (ds_b.right_licznik == 0)
-                {
-                    res.push_back(make_pair(X, 1));
-                    ds_right.left_licznik = ds_b.left_licznik;
-                    ds_right.left_mian = ds_b.left_mian;
-                    ds_right.right_licznik = X;
-                    ds_right.right_mian = 1;
-                    ds_right.licz = ds_b.licz;
-                    ds_right.mian = 2 * ds_b.mian;
-                    pq.push(ds_right);
-                }
-                else
-                {
-                    ds_left.left_licznik = ds_b.left_licznik;
-                    ds_left.left_mian = ds_b.left_mian;
-                    ds_right.right_licznik = ds_b.right_licznik;
-                    ds_right.right_mian = ds_b.right_mian;
-                    nww_b_l = NWW(ds_left.left_mian, ds_b.mian); // wspólny mianownik punktów
-                    nww_b_r = NWW(ds_right.right_mian, ds_b.mian);
-                    // sprawdzenie ile razy trzeba pomnożyć licznik
-                    l_m = nww_b_l / ds_left.left_mian; // lewy
-                    b_m_1 = nww_b_l / ds_b.mian;
-                    //------------------------------//
-                    r_m = nww_b_r / ds_right.right_mian; // prawy
-                    b_m_2 = nww_b_r / ds_b.mian;
-                    //------------------------------// lewy
-                    ds_left.right_licznik = ds_left.left_licznik * l_m + ds_b.licz * b_m_1;
-                    ds_left.right_mian = ds_b.mian;
-                    ds_left.licz = ds_b.licz;
-                    ds_left.mian = ds_b.mian * 2;
-                    //--------------------------// prawy
-                    ds_right.left_licznik = ds_right.right_licznik * r_m - ds_b.licz * b_m_2;
-                    ds_right.left_mian = ds_b.mian;
-                    ds_right.licz = ds_b.licz;
-                    ds_right.mian = ds_b.mian * 2;
-                    //------------------------//
-                    pq.push(ds_left);
-                    pq.push(ds_right);
-                    res.push_back(make_pair(ds_right.left_licznik, ds_right.left_mian));
-                }
-                pq.pop();
+                res.first = X;
+                res.second = 1;
+                ds_right.left_licznik = ds_b.left_licznik;
+                ds_right.left_mian = ds_b.left_mian;
+                ds_right.right_licznik = X;
+                ds_right.right_mian = 1;
+                ds_right.licz = ds_b.licz;
+                ds_right.mian = 2 * ds_b.mian;
+                pq.push(ds_right);
             }
-            lower(res[a - 1]);
-            cout << res[a - 1].first << "/" << res[a - 1].second << "\n";
+            else
+            {
+                ds_left.left_licznik = ds_b.left_licznik;
+                ds_left.left_mian = ds_b.left_mian;
+                ds_right.right_licznik = ds_b.right_licznik;
+                ds_right.right_mian = ds_b.right_mian;
+                nww_b_l = NWW(ds_left.left_mian, ds_b.mian); // wspólny mianownik punktów
+                nww_b_r = NWW(ds_right.right_mian, ds_b.mian);
+                // sprawdzenie ile razy trzeba pomnożyć licznik
+                l_m = nww_b_l / ds_left.left_mian; // lewy
+                b_m_1 = nww_b_l / ds_b.mian;
+                //------------------------------//
+                r_m = nww_b_r / ds_right.right_mian; // prawy
+                b_m_2 = nww_b_r / ds_b.mian;
+                //------------------------------// lewy
+                ds_left.right_licznik = ds_left.left_licznik * l_m + ds_b.licz * b_m_1;
+                ds_left.right_mian = ds_b.mian;
+                ds_left.licz = ds_b.licz;
+                ds_left.mian = ds_b.mian * 2;
+                //--------------------------// prawy
+                ds_right.left_licznik = ds_right.right_licznik * r_m - ds_b.licz * b_m_2;
+                ds_right.left_mian = ds_b.mian;
+                ds_right.licz = ds_b.licz;
+                ds_right.mian = ds_b.mian * 2;
+                //------------------------//
+                pq.push(ds_left);
+                pq.push(ds_right);
+                res.first = ds_right.left_licznik;
+                res.second = ds_right.left_mian;
+                cout << j << "\n";
+            }
+            pq.pop();
         }
+        lower(res);
+        cout << res.first << "/" << res.second << "\n";
+        N = a;
     }
-
     return 0;
 }
