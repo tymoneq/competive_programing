b8fe621513c33410ddd81482bc2c2460f7b3d60c
TyMooNeEQ <tymontumialis@gmail.com>
2022-11-29 22:02:49 +0100

    ple

--- c++/OI_II_etap/ple.cpp
+++ c++/OI_II_etap/ple.cpp
@@ -1,46 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-inline bool sorto(pair<ll, int> &lhs, pair<ll, int> &rhs) { return lhs.first > rhs.first; }
+void upd(int v, int x, vector<ll> &Tree)
+{
+    Tree[v] = x;
+    int tmp = v / 2;
+    while (tmp > 0)
+    {
+        Tree[tmp] = Tree[tmp * 2] + Tree[tmp * 2 + 1];
+        tmp /= 2;
+    }
+}
+ll get_value(int l, vector<ll> &Tree, int base, int n)
+{
+    ll sum = 0;
+    int val = Tree[l];
+    l += 1;
+    if (l == Tree.size())
+        return 0;
+    int r = base + n;
+    sum += Tree[l];
+    while (l / 2 != r / 2)
+    {
+        if (val - sum <= 0)
+            break;
+        if (l % 2 == 0)
+            sum += Tree[l + 1];
+
+        if (r % 2 == 1)
+            sum += Tree[r - 1];
+        l /= 2;
+        r /= 2;
+    }
+    return sum;
+}
+inline bool my_sort(ll &lhs, ll &rhs) { return lhs < rhs; }
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, a;
+    int n;
     cin >> n;
-    vector<int> Weight(n);
-    vector<ll> res;
-    vector<ll> Pref(n, 0);
+    int base = log2(n);
+    if (!(pow(2, base) == n))
+        base++;
+    base = pow(2, base);
+    vector<ll> Tree(base * 2);
+    vector<ll> res(n);
     ll sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> Weight[i];
-        sum += Weight[i];
-    }
-    res.push_back(sum);
-    vector<pair<ll, int>> l_sums;
+    for (int i = base; i < base + n; i++)
+        cin >> Tree[i];
+
+    for (int i = base - 1; i > 0; i--)
+        Tree[i] = Tree[2 * i] + Tree[2 * i + 1];
+
+    sum = Tree[1];
+    res[0] = sum;
     for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
+        for (int j = base; j < base + n; j++)
         {
-            ll left_sum = 0;
-            for (int k = j + 1; k < n; k++)
+            if (Tree[j] == 0)
+                continue;
+            ll left_sum = get_value(j, Tree, base, n);
+            if (Tree[j] - left_sum > 0)
             {
-                left_sum += Weight[k];
-                if ((Weight[j] - left_sum) <= 0)
-                    break;
+                res[i] = res[i - 1] - Tree[j];
+                upd(j, 0, Tree);
+                break;
             }
-            if (Weight[j] - left_sum > 0)
-                l_sums.push_back(make_pair(left_sum, j));
         }
-        sort(l_sums.begin(), l_sums.end(), sorto);
-        auto el = l_sums[0];
-        res.push_back(res[i - 1] - Weight[el.second]);
-        Weight[el.second] = 0;
-        l_sums.clear();
-    }
-    for (int i = n-1; i >= 0; i--)
+
+    sort(res.begin(), res.end(), my_sort);
+    for (int i = 0; i < n; i++)
         cout << res[i] << " ";
     return 0;
 }
