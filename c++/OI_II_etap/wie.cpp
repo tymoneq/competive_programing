d97c0f727900dc5a124c7d5348aa450090e628e7
TyMooNeEQ <tymontumialis@gmail.com>
2022-12-15 17:32:30 +0100

    wie

--- c++/OI_II_etap/wie.cpp
+++ c++/OI_II_etap/wie.cpp
@@ -2,7 +2,7 @@
 // Tymon Tumialis
 using namespace std;
 typedef long long ll;
-constexpr int MAX_VAl = 20;
+constexpr int MAX_VAl = 200000;
 
 struct best_choices
 {
@@ -28,9 +28,9 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int n, h, h1, h2, ben = 1;
+    int n, h, h1, h2, ben = 1, res_to_left = 0, res_to_right = 0;
     best_choices b_ch;
-    ll x, n1, n2, res = 0, prev = 0, left_range = 0, index_left = 0, right_range = 0, right_index = 0;
+    ll x, n1, n2, prev = 0, left_range = 0, index_left = 0, right_range = 0, right_index = 0, total_of_new_domins = 0;
     vector<best_choices> TO_add_left;
     vector<best_choices> To_add_right;
     cin >> n;
@@ -53,6 +53,7 @@ int main()
         prev = x;
     }
     cin >> n1 >> h1 >> n2 >> h2;
+    total_of_new_domins = n1 + n2;
     // Calculating Pref_sum
     for (int i = n - 2; i >= 0; i--)
         Pref_sum_left[i] = Pref_sum_left[i + 1] + cost_of_move[i];
@@ -89,15 +90,25 @@ int main()
         }
     }
     // indexy są pokazują ile potrzeba żeby dany element się przewrócił !!!!
-
+    ll cost = 0;
     for (int i = 1; i < n; i++)
     {
         if (Right_dominos_which_falls[i] != 0)
         {
-            b_ch.benefit = ben;
-            b_ch.cost = Right_dominos_which_falls[i];
+            if (res_to_right == 0)
+            {
+                res_to_right = ben;
+                cost = Right_dominos_which_falls[i];
+            }
+            else
+            {
+                b_ch.benefit = ben;
+                b_ch.cost = cost;
+                To_add_right.push_back(b_ch);
+                cost = Right_dominos_which_falls[i];
+            }
+
             ben = 0;
-            To_add_right.push_back(b_ch);
         }
         ben++;
     }
@@ -106,16 +117,67 @@ int main()
     {
         if (Left_dominos_which_falls[i] != 0)
         {
-            b_ch.benefit = ben;
-            b_ch.cost = Left_dominos_which_falls[i];
+            if (res_to_left == 0)
+            {
+                res_to_left = ben;
+                cost = Left_dominos_which_falls[i];
+            }
+            else
+            {
+                b_ch.benefit = ben;
+                b_ch.cost = cost;
+                cost = Left_dominos_which_falls[i];
+                TO_add_left.push_back(b_ch);
+            }
             ben = 0;
-            TO_add_left.push_back(b_ch);
         }
         ben++;
     }
     // sortowanie które elementy które najlepiej wybrać
     sort(To_add_right.begin(), To_add_right.end(), sort_vector);
     sort(TO_add_left.begin(), TO_add_left.end(), sort_vector);
-
+    ll mod = 0;
+    for (int i = 0; i < To_add_right.size(); i++)
+    {
+        if (To_add_right[i].cost <= total_of_new_domins * h1)
+        {
+            res_to_right += To_add_right[i].benefit;
+            mod = To_add_right[i].cost % h1;
+            res_to_right += To_add_right[i].cost / h1;
+            if (mod != 0)
+            {
+                res_to_right += 1;
+                total_of_new_domins -= 1;
+            }
+            total_of_new_domins -= To_add_right[i].cost / h1;
+        }
+        if (total_of_new_domins == 0)
+            break;
+    }
+    if (total_of_new_domins != 0)
+        res_to_right += total_of_new_domins;
+    total_of_new_domins = n1 + n2;
+    for (int i = 0; i < TO_add_left.size(); i++)
+    {
+        if (TO_add_left[i].cost <= total_of_new_domins * h1)
+        {
+            res_to_left += To_add_right[i].benefit;
+            mod = TO_add_left[i].cost % h1;
+            res_to_left += TO_add_left[i].cost / h1;
+            if (mod != 0)
+            {
+                res_to_left += 1;
+                total_of_new_domins -= 1;
+            }
+            total_of_new_domins -= TO_add_left[i].cost / h1;
+        }
+        if (total_of_new_domins == 0)
+            break;
+    }
+    if (total_of_new_domins != 0)
+        res_to_left += total_of_new_domins;
+    if (n == 1)
+        res_to_left++;
+    cout << max(res_to_left, res_to_right);
     return 0;
 }
