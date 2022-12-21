138a53828dcf29c11f4714712c9f7991223b02fb
TyMooNeEQ <tymontumialis@gmail.com>
2022-12-21 17:27:59 +0100

    graphs

--- c++/OI_II_etap/done/code_forces/boredom.cpp
+++ c++/OI_II_etap/done/code_forces/boredom.cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MAX_VAl = 1e5 + 10;
-long long Sum[MAX_VAl];
+long long num[MAX_VAl];
 long long dp[MAX_VAl];
+long long m[MAX_VAl];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin >> n;
-    vector<int> num(n);
     for (int i = 0; i < n; i++)
+    {
         cin >> num[i];
+        m[num[i]]++;
+    }
 
-    for (int i = 0; i < n; i++)
-        Sum[num[i]] += num[i];
-    dp[1] = Sum[1];
+    dp[0] = 0;
+    dp[1] = m[1];
     for (int i = 2; i < MAX_VAl; i++)
-        dp[i] = max(dp[i - 1], dp[i - 2] + Sum[i]);
+        dp[i] = max(dp[i - 1], dp[i - 2] + m[i] * i);
 
-    cout << dp[n];
+    cout << dp[100002];
     return 0;
 }
---
+++ c++/OI_II_etap/done/code_forces/online_courses.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX_VAL = 1e5 + 10;
+
+vector<int> Graph[MAX_VAL];
+int color[MAX_VAL];
+int Main_courses[MAX_VAL];
+vector<int> res;
+bool cycle = false;
+void dfs(int u)
+{
+    if (color[u] == 0)
+    {
+        color[u] = 1;
+        for (int to : Graph[u])
+            dfs(to);
+        color[u] = 2;
+        res.push_back(u);
+    }
+    else if (color[u] == 1)
+        cycle = true;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, k, a, b, w;
+    cin >> n >> k;
+    for (int i = 0; i < k; i++)
+        cin >> Main_courses[i];
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a;
+        for (int j = 0; j < a; j++)
+        {
+            cin >> b;
+            Graph[i].push_back(b);
+        }
+    }
+    for (int i = 0; i < k; i++)
+        dfs(Main_courses[i]);
+    if (cycle)
+    {
+        cout << -1;
+        return 0;
+    }
+    cout << res.size() << "\n";
+    for (int el : res)
+        cout << el << ' ';
+    return 0;
+}
