4cf1392b77ee0ff8e3ec8d7c4668c62ae6d98522
TyMooNeEQ <tymontumialis@gmail.com>
2022-12-24 12:45:10 +0100

    delivery working to slow

--- c++/OI_II_etap/delivery_dilemma.cpp
+++ c++/OI_II_etap/delivery_dilemma.cpp
@@ -3,50 +3,43 @@
 using namespace std;
 
 const int MAX_VAL = 1e5 + 10;
-int Courier[MAX_VAL];
 pair<int, int> Petya[MAX_VAL];
-bool Visited[MAX_VAL];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int t, n, res = 0, a, tmp = 0, mx = 0;
+    int t, n, res = 0, a, tmp = 0;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        res = 0, tmp = 0, mx = 0;
+        res = 0, tmp = 0;
         cin >> n;
         for (int j = 0; j < n; j++)
-            cin >> Courier[j];
+            cin >> Petya[j].first;
 
         for (int j = 0; j < n; j++)
+            cin >> Petya[j].second;
+
+        sort(Petya, Petya + n, greater<pair<int, int>>());
+        if (Petya[0].first <= Petya[0].second)
+            res = Petya[0].first;
+        else
         {
-            cin >> a;
-            Petya[j] = make_pair(a, j);
-        }
-        sort(Petya, Petya + n);
-        for (int j = 0; j < n; j++)
-        {
-            if (Petya[j].first >= Courier[Petya[j].second])
+            res = Petya[0].second;
+            for (int j = 1; j < n; j++)
             {
-                res = max(res, Courier[Petya[j].second]);
-                continue;
+                if (res >= Petya[j].first)
+                    break;
+
+                if (res + Petya[j].second >= Petya[j].first)
+                {
+                    res = Petya[j].first;
+                    break;
+                }
+                res += Petya[j].second;
             }
-            tmp += Petya[j].first;
-            if (tmp > Courier[Petya[j].second])
-            {
-                for (int k = 0; k < n; k++)
-                    if (!Visited[k])
-                        mx = max(mx, Courier[k]);
-                res = mx;
-                break;
-            }
-            Visited[Petya[j].second] = 1;
-            res = tmp;
         }
-        for (int j = 0; j < n; j++)
-            Visited[j] = 0;
         cout << res << "\n";
     }
     return 0;
