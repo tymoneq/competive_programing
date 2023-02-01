ef8fe0f099c26277e12ba737e5a8c67a5d7a7a87
TyMooNeEQ <tymontumialis@gmail.com>
2023-02-01 22:39:54 +0100

    RoadQueires

--- OI_II_etap/RoadQueris.cpp
+++ OI_II_etap/RoadQueris.cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-constexpr int M = 2e5+10;
+constexpr int M = 2e5 + 10, L = 18;
 struct edge
 {
     int start, end, val;
 };
-
-int R[M], Dist[M];
-bool Vis[M];
+int par[M][L], R[M], Depth[M], Cost[M][L];
 edge E[M];
 vector<pair<int, int>> Mst[M];
 inline int fint(int a)
@@ -22,8 +20,47 @@ inline void onion(int a, int b, int val)
     if (fint(a) == fint(b))
         return;
     R[fint(b)] = fint(a);
-    Mst[b].push_back({a, val});
-    Mst[a].push_back({b, val});
+    Mst[b].emplace_back(a, val);
+    Mst[a].emplace_back(b, val);
+}
+inline void dfs(int v, int p, int d)
+{
+    Depth[v] = d;
+    par[v][0] = p;
+    for (auto w : Mst[v])
+        if (w.first != p)
+        {
+            Cost[w.first][0] = w.second;
+            dfs(w.first, v, d + 1);
+        }
+}
+inline int query(int a, int b)
+{
+    if (Depth[a] > Depth[b])
+        swap(a, b);
+    int ans = 0;
+    for (int d = L - 1; d >= 0; d--)
+        if (Depth[b] - (1 << d) >= Depth[a])
+        {
+            ans = max(ans, Cost[b][d]);
+            b = par[b][d];
+        }
+
+    assert(Depth[a] == Depth[b]);
+    if (a == b)
+        return ans;
+    for (int d = L - 1; d >= 0; d--)
+    {
+        if (par[a][d] != par[b][d])
+        {
+            ans = max(ans, Cost[a][d]);
+            a = par[a][d];
+            ans = max(ans, Cost[b][d]);
+            b = par[b][d];
+        }
+    }
+    ans = max(ans, max(Cost[a][0], Cost[b][0]));
+    return ans;
 }
 int main()
 {
@@ -41,31 +78,27 @@ int main()
     }
     for (int i = 0; i < m; i++)
         onion(E[i].start, E[i].end, E[i].val);
-    queue<int> Q;
+    memset(Depth, -1, sizeof(Depth));
+    memset(par, 0, sizeof(par));
+    memset(Cost, 0, sizeof(Cost));
+
+    for (int i = 1; i <= n; i++)
+        if (Depth[i] == -1)
+            dfs(i, i, 0);
+
+    for (int k = 1; k < L; k++)
+        for (int i = 1; i <= n; i++)
+        {
+            par[i][k] = par[par[i][k - 1]][k - 1];
+            Cost[i][k] = max(Cost[i][k - 1], Cost[par[i][k - 1]][k - 1]);
+        }
     for (int i = 0; i < q; i++)
     {
         cin >> a >> b;
         if (fint(R[a]) != fint(R[b]))
             cout << -1 << "\n";
         else
-        {
-            Q.push(a);
-            for (int j = 0; j <= n; j++)
-                Dist[j] = -1000, Vis[j] = 0;
-            while (!Q.empty())
-            {
-                auto v = Q.front();
-                Q.pop();
-                for (auto w : Mst[v])
-                    if (!Vis[w.first])
-                    {
-                        Dist[w.first] = max(Dist[v], w.second);
-                        Q.push(w.first);
-                        Vis[w.first] = 1;
-                    }
-            }
-            cout << Dist[b] << "\n";
-        }
+            cout << query(a, b) << "\n";
     }
     return 0;
 }
