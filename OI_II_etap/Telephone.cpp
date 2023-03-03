41e48f36c90c3388ddda2b7690e21e7ef8782ca3
Tymoneq <Tymontumialis@gmail.com>
2023-03-03 11:44:46 +0100

    try fix telephone

--- OI_II_etap/Telephone.cpp
+++ OI_II_etap/Telephone.cpp
@@ -2,37 +2,33 @@
 using namespace std;
 constexpr int M = 5e4 + 10, INF = 1e9;
 vector<pair<int, int>> Graph[M];
-int Dist[M];
+int Dist[M], Cows[M];
 bool Vis[M];
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    freopen("6.in", "r", stdin);
     int n, K;
     char z;
     cin >> n >> K;
-    vector<int> Cows(n);
     unordered_map<int, vector<int>> Indexes;
     for (int i = 0; i < n; i++)
     {
         cin >> Cows[i];
         Indexes[Cows[i]].push_back(i);
+        Dist[i] = INF;
     }
     for (int i = 1; i <= K; i++)
         for (int j = 1; j <= K; j++)
         {
             cin >> z;
             if (z == '1')
-                if (Indexes[i].size() > 0)
-                    for (int w : Indexes[i])
-                        if (Indexes[j].size() > 0)
-                            for (int v : Indexes[j])
-                                Graph[w].push_back({v, abs(w - v)});
+                for (int w : Indexes[i])
+                    for (int v : Indexes[j])
+                        if (w != v)
+                            Graph[w].push_back({v, abs(w - v)});
         }
-    for (int i = 0; i < n; i++)
-        Dist[i] = INF;
     Dist[0] = 0;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push({0, 0});
