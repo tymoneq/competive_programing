0065229e7a853f661fb9b800b8296f64b96c01bb
TyMooNeEQ <tymontumialis@gmail.com>
2023-09-25 08:18:55 +0200

    fizyka

--- OI_II_etap/fizyka.cpp
+++ OI_II_etap/fizyka.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 typedef unsigned long long ull;
 // initializacja danych
-constexpr double M = 1, g = 9.81, prec = 0.1; // 1e-16;
-double C[] = {0.1, 0.2, 0.5}, Deltat[] = {0.1, 0.01, 1e-6};
-constexpr int X = 1;
+constexpr double M = 1, g = 9.81, prec = 1e-4;
+double C[] = {0.1, 0.2, 0.5}, Deltat[] = {0.1, 0.01, 1e-4};
+constexpr int X = 3;
 inline double deltaV(double V, double C, double DeltaT) { return (g - (C / M * V)) * DeltaT; };
 
 ofstream Rout, Vout, Sout; // Rout - results Vout - V Sout - S
@@ -22,15 +22,16 @@ int main()
         {
             Vtemp = 0, Sn = 0, Stemp = 0, timer = 1;
             Vn = Vtemp + deltaV(Vtemp, C[i], Deltat[k]);
+            Sn = Stemp + (Vn * Deltat[k]);
             while (Vn - Vtemp >= prec)
             {
                 timer++;
                 Vout << Vtemp << "\n";
                 Sout << Stemp << "\n";
                 Vtemp = Vn;
+                Stemp = Sn;
                 Vn = Vtemp + deltaV(Vtemp, C[i], Deltat[k]);
                 Sn = Stemp + (Vtemp * Deltat[k]);
-                Stemp = Sn;
             }
             Vout << Vtemp << "\n"
                  << Vn << "\n";
--- OI_II_etap/fizyka.py
+++ OI_II_etap/fizyka.py
@@ -1,23 +1,40 @@
 import matplotlib.pyplot as plt
 
-X = 3
 FileS = open("S.txt", "r")
 FileV = open("V.txt", "r")
+point1 = 0
+point2 = 0
 
-V_split = []
-S_split = []
-for V in FileV:
-    if V != "!\n":
-        V_split.append(int(V))
+V = []
+S = []
+for line in FileV:
+    V.append(line)
 
-for S in FileS:
-    if S != "!\n":
-        S_split.append(int(S))
+for line in FileS:
+    S.append(line)
+for i in range(9):
+    V_split = []
+    S_split = []
+    while point1 >= 0:
+        if V[point1] != "!\n":
+            V_split.append(float(V[point1]))
+            point1 += 1
+        else:
+            point1 += 1
+            break
 
+    while point2 >= 0:
+        if S[point2] != "!\n":
+            S_split.append(float(S[point2]))
+            point2 += 1
+        else:
+            point2 += 1
+            break
 
-plt.plot(S_split,V_split)
-plt.xlabel("S")
-plt.ylabel("V")
-plt.grid(True)
-plt.title("fizyka")
-plt.show()
+    plt.plot(S_split, V_split)
+    plt.xlabel("S")
+    plt.ylabel("V")
+    name = 'wykres' + str(i) +'.png'
+    plt.savefig(name)
+    plt.title("V graniczne")
+    plt.show()
