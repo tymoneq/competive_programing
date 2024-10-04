8d9e229c5bd95804e0cfce7c0e4fca941fcd1ccf
tymoneq <tymontumialis@gmail.com>
2024-10-04 14:10:56 +0200

    fff

---
+++ OI_II_etap/troj.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int N = 1010;
+
+vector<int> adj[N];
+bool Vis[N][N];
+int D[N][N];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, a, b;
+    cin >> n >> m;
+
+    for (int i = 0; i <= n; i++)
+        for (int j = 0; j <= n; j++)
+            D[i][j] = -1;
+
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        queue<int> q;
+        q.push(i);
+        Vis[i][i] = 1;
+        D[i][i] = 0;
+        while (!q.empty())
+        {
+            int v = q.front();
+            q.pop();
+            for (int w : adj[v])
+                if (!Vis[i][w])
+                {
+                    D[i][w] = D[i][v] + 1;
+                    Vis[i][w] = 1;
+                    q.push(w);
+                }
+        }
+    }
+
+    
+    return 0;
+}
