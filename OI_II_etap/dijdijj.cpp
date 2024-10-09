3c77d1d094250c0a9b71c28928061579abc0f26c
tymoneq <tymontumialis@gmail.com>
2024-10-09 18:29:27 +0200

    wiedzmak

--- OI_II_etap/dijdijj.cpp
+++ OI_II_etap/dijdijj.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 typedef long long ll;
-constexpr int N = 1e5 + 10;
+constexpr int N = 20;
 ll D[N];
 bool Vis[N];
-vector<pair<int, int>> adj[N];
 struct edge
 {
-    int a, b, c;
+    int end, cost, indx;
 };
-pair<int, int> Parent[N];
+vector<edge> adj[N];
+pair<int, int> P[N];
 
 int main()
 {
@@ -20,19 +20,21 @@ int main()
     cout.tie(0);
 
     int n, m, a, b, c;
+    ll res = 0;
     cin >> n >> m;
 
-    while (m--)
+    for (int i = 0; i < m; i++)
     {
         cin >> a >> b >> c;
-        adj[a].push_back({b, c});
-        adj[b].push_back({a, c});
+        adj[a].push_back({b, c, i});
+        adj[b].push_back({a, c, i});
     }
 
     for (int i = 1; i <= n; i++)
         D[i] = numeric_limits<ll>::max();
     priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
     pq.push({0, 1});
+    P[1] = {1, 0};
     D[1] = 0;
     while (!pq.empty())
     {
@@ -41,16 +43,53 @@ int main()
         if (!Vis[v])
         {
             for (auto w : adj[v])
-                if (D[w.first] > D[v] + w.second)
+                if (D[w.end] > D[v] + w.cost)
                 {
-                    D[w.first] = D[v] + w.second;
-                    Parent[w.first] = {v, w.second};
-                    pq.push({D[w.first], w.first});
+                    D[w.end] = D[v] + w.cost;
+                    P[w.end] = {v, w.indx};
+                    pq.push({D[w.end], w.end});
                 }
+
+            Vis[v] = 1;
+        }
+    }
+    res += D[n];
+    set<int> UsedEdges;
+    int p;
+    p = n;
+    while (P[p].first != p)
+    {
+        UsedEdges.insert({P[p].second});
+        p = P[p].first;
+    }
+    UsedEdges.insert(P[p].second);
+    for (int i = 1; i <= n; i++)
+    {
+        D[i] = numeric_limits<ll>::max();
+        Vis[i] = 0;
+    }
+    D[1] = 0;
+    pq.push({0, 1});
+    while (!pq.empty())
+    {
+        int v = pq.top().second;
+        pq.pop();
+        if (!Vis[v])
+        {
+            for (auto w : adj[v])
+                if (D[w.end] > D[v] + w.cost && UsedEdges.find(w.indx) == UsedEdges.end())
+                {
+                    D[w.end] = D[v] + w.cost;
+                    P[w.end] = {v, w.indx};
+                    pq.push({D[w.end], w.end});
+                }
+            Vis[v] = 1;
         }
     }
 
-    pair<int, int> p = {n, 0};
-    set<edge> E;
+    if (D[n] == numeric_limits<ll>::max())
+        cout << -1 << "\n";
+    else
+        cout << D[n] + res << "\n";
     return 0;
 }
---
+++ OI_II_etap/wiedzmaak.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr int N = 210, BASE = 1 << 15;
+typedef long long ll;
+
+struct edge
+{
+    int end, cost;
+    int monsters = BASE;
+};
+
+struct wiedzmak
+{
+    ll time;
+    int v;
+    int Swords = BASE;
+};
+
+vector<edge> adj[N];
+wiedzmak D[N];
+int Kowal[N];
+
+struct wiedzmakComparator
+{
+
+    bool operator()(wiedzmak &lhs, wiedzmak &rhs)
+    {
+        return lhs.time > rhs.time;
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m, p, k, a, b, c, d, e;
+    cin >> n >> m >> p >> k;
+    for (int i = 1; i <= n; i++)
+    {
+        D[i].time = numeric_limits<ll>::max();
+        Kowal[i] = BASE;
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> a >> b;
+        while (b--)
+        {
+            cin >> c;
+            int C = 1 << c;
+            Kowal[a] |= C;
+        }
+    }
+    while (m--)
+    {
+        cin >> a >> b >> c >> d;
+
+        int A = BASE;
+        while (d--)
+        {
+            cin >> e;
+            int E = 1 << e;
+            A |= E;
+        }
+        adj[a].push_back({b, c, A});
+        adj[b].push_back({a, c, A});
+    }
+
+    priority_queue<wiedzmak, vector<wiedzmak>, wiedzmakComparator> pq;
+
+    int A = BASE | Kowal[1];
+
+    pq.push({0, 1, A});
+    D[1].time = 0;
+    D[1].Swords = A;
+    ll res = numeric_limits<ll>::max();
+    while (!pq.empty())
+    {
+        auto v = pq.top();
+        pq.pop();
+        if (v.v == n)
+            res = min(res, D[v.v].time);
+
+        for (auto w : adj[v.v])
+            if ((w.monsters & v.Swords) == w.monsters)
+            {
+                if (D[w.end].time > w.cost + D[v.v].time)
+                {
+                    D[w.end].time = w.cost + D[v.v].time;
+                    D[w.end].Swords = v.Swords | Kowal[w.end];
+                    pq.push({D[w.end].time, w.end, (v.Swords | Kowal[w.end])});
+                }
+                else if ((v.Swords | D[w.end].Swords) != D[w.end].Swords)
+                {
+                    D[w.end].time = w.cost + D[v.v].time;
+                    D[w.end].Swords = v.Swords | Kowal[w.end];
+                    pq.push({D[w.end].time, w.end, (v.Swords | Kowal[w.end])});
+                }
+                if (w.end == n)
+                    res = min(res, D[w.end].time);
+            }
+    }
+    if (res == numeric_limits<ll>::max())
+        cout << -1 << "\n";
+    else
+        cout << res << "\n";
+    return 0;
+}
