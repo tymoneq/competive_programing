6a301e654c0e3860441b352be8f35450920c6931
tymoneq <tymontumialis@gmail.com>
2024-11-04 19:32:04 +0100

    jebać stare kurwy prądem

--- OI_II_etap/done/POI/wagony.cpp
+++ OI_II_etap/done/POI/wagony.cpp
@@ -1,16 +1,47 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+typedef unsigned long long ull;
 typedef long long ll;
-constexpr int N = 1e5 + 10;
-constexpr ll INF = 1e17;
 
-ll n, a, b, d;
-ll dp[N];
+ll N, d;
+ull a, b;
 
-inline ll f(ll w, ll v)
+unordered_map<ll, ull> dp;
+
+inline ull f(ull w, ull v) { return (a * w + b * v) % 1001; }
+
+void calc(ll left, ll right)
 {
-    return (w * a + b * v) % 1001;
+    ll newleft = (left - d + 1) / 2;
+    ll newRight = (right + d) / 2;
+    // rekurencja
+    if (left >= d)
+        calc(newleft, newRight);
+
+    if (left < d && left > 1)
+        calc(1, newRight);
+
+    // Obliczanie wyniku
+    for (ll i = max(left, (ll)2); i <= right; i++)
+    {
+        if (dp.find(i) != dp.end())
+            continue;
+        dp[i] = numeric_limits<ull>::max();
+        for (ll j = max(newleft, (ll)1); j <= newRight; j++)
+        {
+            ll x = i - j;
+            if (abs(x - j) > d)
+                continue;
+
+            if (x <= 0)
+                break;
+
+            ull mn = min(f(x, j), f(j, x)) + dp[x] + dp[j];
+            dp[i] = min(dp[i], mn);
+        }
+    }
 }
 
 int main()
@@ -19,22 +50,12 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
-    cin >> n >> d >> a >> b;
-    for (int i = 0; i <= n; i++)
-        dp[i] = INF;
+    cin >> N >> d >> a >> b;
 
-    dp[1] = 0;
+    dp[0] = 0, dp[1] = 0;
 
-    for (int x = 2; x <= n; x++)
-    {
-        for (int i = (x - d + 1) / 2; i <= (x + d) / 2; i++)
-        {
-            int j = x - i;
-            if (j < 0 || i < 0)
-                continue;
-            dp[x] = min(dp[x], dp[i] + dp[j] + f(i, j));
-        }
-    }
-    cout << dp[n] << "\n";
+    calc(N, N);
+
+    cout << dp[N] << "\n";
     return 0;
 }
---
+++ OI_II_etap/done/POI/wagonybrut.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+constexpr int N = 1e5 + 10;
+constexpr ll INF = 1e17;
+
+ll n, a, b, d;
+ll dp[N];
+
+inline ll f(ll w, ll v)
+{
+    return (w * a + b * v) % 1001;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> d >> a >> b;
+    for (int i = 0; i <= n; i++)
+        dp[i] = INF;
+
+    dp[1] = 0;
+
+    for (int x = 2; x <= n; x++)
+    {
+        for (int i = (x - d + 1) / 2; i <= (x + d) / 2; i++)
+        {
+            int j = x - i;
+            if (j < 0 || i < 0)
+                continue;
+            dp[x] = min(dp[x], dp[i] + dp[j] + f(i, j));
+        }
+    }
+
+    for (int i = 0; i <= n; i++)
+        cout << dp[i] << "\n";
+    cout << dp[n] << "\n";
+    return 0;
+}
